ffe6a0eaa99cda610623a5f82063cc752055263d
luke358 <qq1494135711@gmail.com>
2024-03-14 15:21:19 +0800

    fix: fix

--- lib/ui/diary_list.dart
+++ lib/ui/diary_list.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 import 'package:random_note/main.dart';
 import 'package:random_note/models/diary.dart';
